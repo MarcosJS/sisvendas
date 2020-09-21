a989d41b0bf6d839788822a120b0d77636621c27
Marcos Jose <marcos.com.j.s@gmail.com>
2020-09-21 19:21:59 +0000

    Ateracao no arquivo database.php para tratar erro de conexao

--- config/database.php
+++ config/database.php
@@ -68,6 +68,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
+            'url' => $DATABASE_URL["url"],
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"], '/'),
