e630cd787072ec37a385a4f1336c70a9702a18c3
MarcosJS <marcos.com.j.s@gmail.com>
2020-10-12 21:02:05 +0000

    Atualização do deploy

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
             ]) : [],
         ],
 
-        /*'pgsql' => [
+        'pgsql' => [
             'driver' => 'pgsql',
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
@@ -79,10 +79,10 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],*/
+        ],
 
         //substituir quando estiver no ambiente local
-        'pgsql' => [
+       /*'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -95,7 +95,7 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
+        ],*/
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
